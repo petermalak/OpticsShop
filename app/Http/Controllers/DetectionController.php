5d5dde1dca9847e396134ff18f628b27b5372834
Peter Malak <pmalak14@gmail.com>
2021-01-27 22:02:57 +0200

    adding Client & Detection model & controller

---
+++ app/Http/Controllers/DetectionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detection;
+use Illuminate\Http\Request;
+
+class DetectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Detection  $detection
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Detection $detection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Detection  $detection
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Detection $detection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Detection  $detection
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Detection $detection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Detection  $detection
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Detection $detection)
+    {
+        //
+    }
+}
